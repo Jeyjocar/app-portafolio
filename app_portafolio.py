5341121e0e244de08e6c6553d303f04b91bff276
Jeyfrey Johan Calero Rojas <jeyjoca@hotmail.com>
2024-03-09 20:55:17 -0500

    Update app_portafolio.py

--- app_portafolio.py
+++ app_portafolio.py
@@ -5,7 +5,6 @@ from streamlit_lottie import st_lottie
 import base64
 from PIL import Image
 import io
-
 import folium
 from streamlit_folium import folium_static
 
@@ -17,9 +16,6 @@ st.set_page_config(page_title='my portfolio', page_icon=':wave:', layout='wide')
 
 # Obtener la ubicación del usuario (opcional)
 # location = st.location()
-
-
-
 
 background_image = """
 <style>
@@ -41,8 +37,8 @@ def load_lottie(url):
     return response.json()
 
 # Temas
-themes = ["Claro", "Oscuro","fondo_imagen"]
-selected_theme = st.sidebar.radio("Selecciona un tema", themes)
+themes = ["Light", "Dark","background image"]
+selected_theme = st.sidebar.radio("Choose theme", themes)
 # Cambiar el tema de la aplicación
 if selected_theme == "Oscuro":
     st.write("""
@@ -73,12 +69,7 @@ else:
 }   
     </style>
 
-
 """, unsafe_allow_html=True)
-
-
-
-
 
 # Use local CSS
 def local_css(file_name):
@@ -86,11 +77,6 @@ def local_css(file_name):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("estilos/estilos.css")
-
-
-
-
-
 
 codigo_lottie= load_lottie("https://lottie.host/f6b9cd22-dde0-4035-9644-e2f47aff0432/hUS0bTQwi3.json")
 codigo_lottie_proyectos= load_lottie("https://lottie.host/695aeef8-d4b0-415d-a016-00c3d9c5b0a8/hKURFkbXbT.json")
@@ -103,16 +89,13 @@ github_lottie = load_lottie("https://assets8.lottiefiles.com/packages/lf20_6HFXX
 docker_lottie = load_lottie("https://assets4.lottiefiles.com/private_files/lf30_35uv2spq.json")
 
 st.write('##')
-st.subheader('Bienvenidos  A mi PORTAFOLIO :smile:')
+st.subheader('Welcome to my PORTAFOLIO :smile:wave:')
 
-st.write('''Mi  nombre  es  Juan, actualmente soy el ceo y fundador
-         y  de una startup de inteligencia artificial y realidad aumentada, Recuerda,
-         la palabra imposible borrala de Tu Vocabulario, 
-         y lograras lo impensado!...''')
+st.write('''My name is Jeyfrey Calero, recently I am studying programming and I had learned
+         about artificial intelligence, Databases Postgres-Mongo, Scrapping, IOT, Python, Javascript, Streamlit
+         and frameworks like Django I love programm, I love challenges and learning about new programming languages. ''')
 
 st.write('----')
-
-
 
 
 #animacion 1 Cargar imágenes desde el sistema de archivos
@@ -125,17 +108,17 @@ image4 = Image.open("img/img4.jpg")
 images = [image1, image2, image3, image4]
 
 # Título del slider carousel
-st.title("Slider Carousel con Streamlit")
+st.title("Slider Carousel with Streamlit")
 
 # Seleccionar la imagen actual mediante un slider
-image_index = st.slider("Selecciona una imagen", 0, len(images)-1, 0)
+image_index = st.slider("Choose an image", 0, len(images)-1, 0)
 
 # Mostrar la imagen actual
 st.image(images[image_index], use_column_width=True)
 
 #animacion 2 Cargar imágenes desde el sistema de archivos
 
-uploaded_files = st.file_uploader("Cargar imágenes", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("Charge image", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
 
 # Verificar si se han cargado archivos
 if uploaded_files:
@@ -146,7 +129,7 @@ if uploaded_files:
     st.title("Slider Carousel con Streamlit")
 
     # Seleccionar la imagen actual mediante un slider
-    image_index = st.slider("Selecciona una imagen", 0, len(images)-1, 0)
+    image_index = st.slider("Choose image", 0, len(images)-1, 0)
 
     # Mostrar la imagen actual
     st.image(images[image_index], use_column_width=True)
@@ -155,40 +138,38 @@ if uploaded_files:
 with st.container():
     selected = option_menu(
         menu_title= None,
-        options= ['Acerca de mi',  'Contactame!..','Curriculum','skills'],
+        options= ['About me',  'Contact me!..','Curriculum','Skills'],
         icons=['person-check', 'code-slash', 'chat'],
         orientation = 'horizontal',
     )
 
-if selected == 'Acerca de mi':
+if selected == 'About me':
     with st.container():
         col1,col2= st.columns(2)
         with col1:
             st.write("##")
-            st.subheader("Hola MI nombre es Juan  Quintero!")
+            st.subheader("Hi my name is Jeyfrey Calero!")
             st.title("""
-                     Soy un apasionado por la tecnologia, la inteligencia artificial 
-                     el analisis de datos y desarollo web Fullstack Es un placer conocerlos!
-                     y aun mas que cada vez nuestra comunidad de programadores crece a pasos
-                     agigantados! 
+                     I am passioned by the technologist, artificial intelligence, 
+                     web development Fullstack, it is a pleasure to meet you!
                      """)
         with col2:
             st_lottie(codigo_lottie)
 
 
 
-if selected == 'Contactame!..':
+if selected == 'Contact me!..':
     with st.container():
         st.write("---")
-        st.header("Enviame Un Mensaje!")
+        st.header("Send a message!")
         st.write("##")
 
         contact_form = """
         <form action="https://formsubmit.co/jvera02@itfip.edu.co" method="POST">
             <input type="hidden" name="_captcha" value="false">
-            <input type="text" name="name" placeholder="Tu Nombre" required>
-            <input type="text" name="email" placeholder="Tu email" required>
-            <textarea name="message" placeholder="Ingresa Tu Mensaje" required></textarea>
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="text" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Ingresa Your menssage" required></textarea>
             <button type="submit">Enviar</button>
         </form>
         """
@@ -198,7 +179,7 @@ if selected == 'Contactame!..':
         with right_column:
             st_lottie(codigo_lottie)
         
-        st.header("Mi Ubicacion")
+        st.header("My location")
         
        # Crear un mapa centrado en la ubicación del usuario
         m = folium.Map(location=[4.43889, -75.23222], zoom_start=10)
@@ -235,18 +216,17 @@ if selected == 'Curriculum':
             href = f'<a href="data:application/octet-stream;base64,{bin_file.decode()}" download="{file_name}">{file_label}</a>'
             return href
 
-    st.markdown(get_binary_file_downloader_html(pdf_data, file_label='Descargar PDF', file_name='doc_aplicaciones.pdf'), unsafe_allow_html=True)
+    st.markdown(get_binary_file_downloader_html(pdf_data, file_label='Download CV PDF', file_name='doc_aplicaciones.pdf'), unsafe_allow_html=True)
 
-if selected == 'skills':
+if selected == 'Skills':
        with st.container():
         col1,col2= st.columns(2)
         with col1:
             st.write("##")
             st.subheader("Python")
-            st.write(""" Mis conocimientos con este lenguaje son avanzados,
-            dado que he logrado implementar aplicaciones web utilizando django y flask,
-            al mismo tiempo aplicaciones de analisis de datos e inteligencia artificial usando
-                     Streamlit""")
+            st.write(""" My Knowledge about Python are intemediate to advance,
+            I have implemented web applications using Django Framework,
+            and anothers applications using artificial intelligence and Streamlit""")
         with col2:
             st_lottie(python_lottie)
         
@@ -256,11 +236,9 @@ if selected == 'skills':
             col1,col2= st.columns(2)
             with col1:
                 st.write("##")
-                st.subheader("javascript")
-                st.write(""" Mis conocimientos con este lenguaje son de un nivel 
-                          intermedio avanzado,
-                           dado que he logrado implementar aplicaciones web utilizando react y angular,vue
-                          al mismo tiempo aplicaciones Moviles con react Native""")
+                st.subheader("Javascript")
+                st.write("""My knoleagment with Javascript are basic to intermediate,
+                        however I am continue learning""")
             with col2:
                 st_lottie(js_lottie)
         st.markdown("---")
@@ -269,11 +247,9 @@ if selected == 'skills':
             col1,col2= st.columns(2)
             with col1:
                 st.write("##")
-                st.subheader("java")
-                st.write(""" Mis conocimientos con este lenguaje son de un nivel 
-                          intermedio avanzado,
-                           dado que he logrado implementar aplicaciones web utilizando Spring y Hibernate ,
-                          al mismo tiempo aplicaciones Moviles con Kotlin y Android Estudio""")
+                st.subheader("Streamlit")
+                st.write("""My knowledge with this language is from a basic to intermediate level, 
+                since I have managed to implement some web applications with artificial intelligence.""")
             with col2:
                 st_lottie(java_lottie)
         
@@ -293,10 +269,9 @@ if selected == 'skills':
                 with col1:
                     st.write("##")
                     st.subheader("Git")
-                    st.write(""" Mis conocimientos con Git son de un nivel 
-                            avanzado,
-                            dado que he logrado implementar la clonacion y migracion de distintas aplicaciones utilizando commit
-                            y master""")
+                    st.write("""My knowledge with Github is at an intermediate level. 
+                            advanced level, since I have managed to implement
+                            migrations of different repositories using commit""")
                 with col2:
                     st_lottie(git_lottie)
 
@@ -305,10 +280,9 @@ if selected == 'skills':
                 with col1:
                     st.write("##")
                     st.subheader("Github")
-                    st.write(""" Mis conocimientos con Github  son de un nivel 
-                            avanzado,
-                            dado que he logrado implementar la clonacion y migracion de distintos repositorios  utilizando commit
-                            y master""")
+                    st.write(""" My knowledge with Github is at an intermediate level. 
+                            advanced level, since I have managed to implement
+                            migrations of different repositories using commit""")
                 with col2:
                     st_lottie(github_lottie)
 
